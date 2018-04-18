4f1c7a33520def97a3ed32a851ed327216cea68e
Jennifer Williams <jennifer@squareandplum.com>
2018-04-17 19:17:54 -0700

    add tests and update .eslintrc.json

--- shift-array.js
+++ shift-array.js
@@ -1,17 +1,20 @@
 'use strict';
 
-function insertShiftArray (inputArray, newValue) {
+const shift = module.exports = {};
+
+shift.insertShiftArray = (inputArray, newValue) => {
   const middle = Math.ceil(inputArray.length / 2);
   const outputArray = [];
 
-  for (let i = 0; i < middle; i += 1) {
+  for (let i = 0; i < middle; i++) {
     outputArray[i] = inputArray[i];
   }
   outputArray[middle] = newValue;
   let j = middle;
-  for (let i = j + 1; i < inputArray.length + 1; i += 1) {
+  for (let i = j + 1; i < inputArray.length + 1; i++) {
     outputArray[i] = inputArray[j];
     j += 1;
   }
   return outputArray;
-}
+};
+
