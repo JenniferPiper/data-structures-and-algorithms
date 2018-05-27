3fb3bcbc869570b06a14cb55f8c7bf438fa5f4e0
Jennifer Williams <jennifer@squareandplum.com>
2018-05-26 20:15:11 -0700

    fix find-match and add some tests. Needs more tests and code cleanup

--- __test__/find-matches.test.js
+++ __test__/find-matches.test.js
@@ -1,5 +1,6 @@
 'use strict';
 
+const util = require('util');
 const KaryTree = require('../find-matches/kary-tree');
 const Node = require('../find-matches/node');
 const traversal = require('../find-matches/find-matches');
@@ -8,12 +9,24 @@ describe('treeA', () => {
   const nodeA = new Node(1);
   const nodeB = new Node(2);
   const nodeC = new Node(3);
+  const nodeD = new Node(2);
+  const nodeE = new Node(2);
+  const nodeF = new Node(2);
+  const nodeG = new Node(7);
 
   const treeA = new KaryTree(nodeA);
 
-  nodeA.children = [nodeB, nodeC];
+  nodeA.children = [nodeB, nodeC, nodeD];
+  nodeB.children = [nodeF, nodeG];
+  nodeD.children = [nodeE];
+
+  console.log('findMatches(treeA.root, 2)', util.inspect(traversal.findMatches(treeA.root, 2), false, null));
+
 
   test('treeA', () => {
-    expect(traversal.findMatches(treeA.root)).toEqual('1\n23\n');
+    expect(traversal.findMatches(treeA.root, 2).head.value.value).toEqual(2);
+    expect(traversal.findMatches(treeA.root, 7).head.value.value).toEqual(7);
+    expect(traversal.findMatches(treeA.root, 5).head).toEqual(null);
+   // expect(traversal.findMatches(treeA.root, 5).length()).toEqual(3);
   });
 });
--- find-matches/find-matches.js
+++ find-matches/find-matches.js
@@ -1,5 +1,7 @@
 'use strict';
 
+const util = require('util');
+
 class LlNode {
   constructor(value) {
     this.value = value;
@@ -11,27 +13,43 @@ class LinkedList {
   constructor() {
     this.head = null;
   }
-
   insertAtHead(value) {
     const node = new LlNode(value);
-
     node.next = this.head;
     this.head = node;
     return this;
+  }
+  length() {
+    let count = 0;
+    if (this.head === null) {
+      let current = this.head;
+      while (current.next != null) {
+        current = current.next;
+        count += 1;
+      }
+    }
+    return count;
   }
 }
 const traversal = module.exports = {};
 
 traversal.findMatches = (rootNode, targetValue) => {
   const matches = new LinkedList();
-
+  let temp = 0;
   const traverseTree = (current) => {
+    console.log('current:', util.inspect(current, false, null));
+
     if (current.value === targetValue) {
       matches.insertAtHead(current);
-      for (let x = 0; x < current.children.length; x++) {
-        traverseTree(current.children[x]);
-      }
     }
+    if (current.children === []) {
+      return undefined;
+    }
+    for (let x = 0; x < current.children.length; x++) {
+      console.log(`current.children[${x}]`, util.inspect(current.children[x], false, null));
+      traverseTree(current.children[x]);
+    }
+    temp += 1;
   };
   traverseTree(rootNode);
   return matches;
